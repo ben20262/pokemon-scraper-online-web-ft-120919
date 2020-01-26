6a27421cc806363ecb880a5ef45696c17ed1ca18
Benjamin Small <ben20262@aim.com>
2020-01-26 00:42:52 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -9,10 +9,20 @@ class Pokemon
     @db = db
   end
 
-  def self.save(name, type, db)
+  def self.save (name, type, db)
     sql = <<-SQL
       INSERT INTO pokemon (name, type) VALUES (?, ?)
     SQL
     db.execute(sql, name, type)
+  end
+
+  def self.find (id, db)
+    sql = <<-SQL
+      SELECT *
+      FROM pokemon
+      WHERE id = ?
+    SQL
+    array = db.execute(sql, id).first
+    student = self.new(id: array[0], name: array[1], type: array[2], db: db)
   end
 end
